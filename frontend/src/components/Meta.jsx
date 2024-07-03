a77f1d8e611b6a1b672454f034cbd38c4b7767b7
ankit <ankithkumar563@gmail.com>
2024-07-03 11:22:44 +0530

    Some changes made

--- frontend/src/components/Meta.jsx
+++ frontend/src/components/Meta.jsx
@@ -11,9 +11,9 @@ const Meta = ({ title, description, keywords }) => {
 };
 
 Meta.defaultProps = {
-  title: 'Welcome To ProShop',
-  description: 'We sell the best products for cheap',
-  keywords: 'electronics, buy electronics, cheap electroincs',
+  title: 'Welcome To Agricopia',
+  description: 'Suggestion regarding crops',
+  keywords: 'crops, fertilizer, cheap, prices',
 };
 
 export default Meta;
--- frontend/src/components/Product.jsx
+++ frontend/src/components/Product.jsx
@@ -23,7 +23,7 @@ const Product = ({ product }) => {
           />
         </Card.Text>
 
-        <Card.Text as='h3'>Rs.{product.price}</Card.Text>
+        <Card.Text as='h3'>₹{product.price}</Card.Text>
       </Card.Body>
     </Card>
   );
